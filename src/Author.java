751fde0cf09a08af527ecf594798597b94948769
amenbruhh <pachison.artyom@yandex.ru>
2024-07-12 18:33:44 +0300

    commit

--- src/Author.java
+++ src/Author.java
@@ -14,12 +14,4 @@ public class Author {
     public String getLastName() {
         return lastName;
     }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
 }
